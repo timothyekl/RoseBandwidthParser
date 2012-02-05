2403d9f2224d47d29c2f2b81c94900aafb59a872
Tim Ekl <lithium3141@gmail.com>
2012-02-04 23:18:10 -0500

    Hit 100% test coverage! woo
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- RoseBandwidthParserTests/RBTotalUsageRecordTests.m
+++ RoseBandwidthParserTests/RBTotalUsageRecordTests.m
@@ -20,4 +20,18 @@
     STAssertEquals(record.machineRecords.count, (NSUInteger)0, @"Empty total usage record had machine usage record");
 }
 
+- (void)testGettersSetters {
+    RBUsagePolicy * policy = [[RBUsagePolicy2012 alloc] init];
+    RBTotalUsageRecord * record = [[RBTotalUsageRecord alloc] initWithPolicy:policy];
+    
+    for(float x = 0.0f; x < 5.0f; x += 0.1f) {
+        record.policyDown = record.policyUp = record.actualDown = record.actualUp = x;
+        STAssertEquals(record.policyDown, x, @"Policy down not saved accurately");
+        STAssertEquals(record.policyUp, x, @"Policy up not saved accurately");
+        STAssertEquals(record.actualDown, x, @"Actual down not saved accurately");
+        STAssertEquals(record.actualUp, x, @"Actual up not saved accurately");
+    }
+    
+}
+
 @end
